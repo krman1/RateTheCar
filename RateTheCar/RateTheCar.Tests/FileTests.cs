5582501fffcef9f3c38d97572ed6a4757112ea1b
krman1 <mar.aniol@interia.pl>
2023-05-06 19:24:09 +0200

    refactor

---
+++ RateTheCar/RateTheCar.Tests/FileTests.cs
@@ -0,0 +1,80 @@
+﻿namespace RateTheCar.Tests
+{
+    public class FileTests
+    {
+
+        [Test]
+        public void GetStatisticsMaxTest()
+        {
+            // arrange
+            CarInFile carInFile = new CarInFile("Ford", "Mustang");
+            carInFile.AddScore(5);
+            carInFile.AddScore(7);
+            carInFile.AddScore(3);
+            carInFile.AddScore(8);
+            carInFile.AddScore(1);
+
+            // act
+            var statistics = carInFile.GetStatistics();
+            var result = statistics.Max;
+
+            // assert
+            Assert.AreEqual(8, result);
+        }
+        [Test]
+        public void GetStatisticsMinTest()
+        {
+            // arrange
+            CarInFile carInFile = new CarInFile("Ford", "Mustang");
+            carInFile.AddScore(5);
+            carInFile.AddScore(7);
+            carInFile.AddScore(3);
+            carInFile.AddScore(8);
+            carInFile.AddScore(1);
+
+            // act
+            var statistics = carInFile.GetStatistics();
+            var result = statistics.Min;
+
+            // assert
+            Assert.AreEqual(1, result);
+        }
+        [Test]
+        public void GetStatisticsAverageTest()
+        {
+            // arrange
+            CarInFile carInFile = new CarInFile("Ford", "Mustang");
+            carInFile.AddScore(5);
+            carInFile.AddScore(7);
+            carInFile.AddScore(3);
+            carInFile.AddScore(8);
+            carInFile.AddScore(1);
+
+            // act
+            var statistics = carInFile.GetStatistics();
+            var result = Math.Round(statistics.Average);
+
+            // assert
+            Assert.AreEqual(5, result);
+        }
+        [Test]
+        public void GetStatisticsAverageLetterTest()
+        {
+            // arrange
+            CarInFile carInFile = new CarInFile("Ford", "Mustang");
+            carInFile.AddScore(5);
+            carInFile.AddScore(7);
+            carInFile.AddScore(3);
+            carInFile.AddScore(8);
+            carInFile.AddScore(1);
+
+            // act
+            var statistics = carInFile.GetStatistics();
+            var result = statistics.AverageLetter;
+
+            // assert
+            Assert.AreEqual('C', result);
+        }
+    }
+}
+
